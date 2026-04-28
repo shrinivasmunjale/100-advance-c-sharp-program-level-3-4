1bff84767ddb53cc325ed77966006494e5104993
shrinivasmunjale <shrinivasmunjale3@gmail.com>
2026-04-29 00:37:05 +0530

    advance programs

---
+++ 159.ERPSystem/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Employee
+{
+    public int Id;
+    public string Name;
+}
+
+class Product
+{
+    public int Id;
+    public string Name;
+    public int Quantity;
+}
+
+class ERPSystem
+{
+    List<Employee> employees = new List<Employee>();
+    List<Product> products = new List<Product>();
+
+    public void ShowMenu()
+    {
+        Console.WriteLine("1. Employee");
+        Console.WriteLine("2. Inventory");
+        Console.WriteLine("3. Billing");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ERPSystem erp = new ERPSystem();
+        erp.ShowMenu();
+    }
+}
