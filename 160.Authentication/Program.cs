23d879bef715fd9416cb015e6569e229203fa23c
shrinivasmunjale <shrinivasmunjale3@gmail.com>
2026-04-29 00:37:21 +0530

    advance programs

---
+++ 160.Authentication/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+class User
+{
+    public string Username;
+    public string Password;
+    public string Role;
+}
+
+class Program
+{
+    static List<User> users = new List<User>()
+    {
+        new User { Username="admin", Password="123", Role="Admin" },
+        new User { Username="user", Password="123", Role="User" }
+    };
+
+    static void Main()
+    {
+        Console.Write("Enter Username: ");
+        string uname = Console.ReadLine();
+
+        Console.Write("Enter Password: ");
+        string pass = Console.ReadLine();
+
+        var user = users.Find(u => u.Username == uname && u.Password == pass);
+
+        if (user != null)
+        {
+            Console.WriteLine("Login Successful!");
+
+            if (user.Role == "Admin")
+            {
+                Console.WriteLine("Welcome Admin! You can manage system.");
+            }
+            else
+            {
+                Console.WriteLine("Welcome User! Limited access.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid Login!");
+        }
+    }
+}
