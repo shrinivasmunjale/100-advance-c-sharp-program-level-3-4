e57487e93c650417c007566a312325d85e224b16
shrinivasmunjale <shrinivasmunjale3@gmail.com>
2026-04-29 00:36:16 +0530

    advance programs

---
+++ 156.CRUDApplication/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static string path = "data.txt";
+
+    static void Main()
+    {
+        int choice;
+
+        do
+        {
+            Console.WriteLine("\n1. Add\n2. View\n3. Delete All\n4. Exit");
+            choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    AddData();
+                    break;
+                case 2:
+                    ViewData();
+                    break;
+                case 3:
+                    DeleteData();
+                    break;
+            }
+
+        } while (choice != 4);
+    }
+
+    static void AddData()
+    {
+        Console.Write("Enter text: ");
+        string data = Console.ReadLine();
+
+        File.AppendAllText(path, data + Environment.NewLine);
+    }
+
+    static void ViewData()
+    {
+        if (File.Exists(path))
+        {
+            string content = File.ReadAllText(path);
+            Console.WriteLine("\nFile Content:\n" + content);
+        }
+        else
+        {
+            Console.WriteLine("File not found");
+        }
+    }
+
+    static void DeleteData()
+    {
+        if (File.Exists(path))
+        {
+            File.Delete(path);
+            Console.WriteLine("File Deleted");
+        }
+    }
+}
