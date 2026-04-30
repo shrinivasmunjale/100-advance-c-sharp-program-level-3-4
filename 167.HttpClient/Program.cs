4e3bf33adedafaba9a9beafbd390272552dcbce8
shrinivasmunjale <shrinivasmunjale3@gmail.com>
2026-04-30 09:34:47 +0530

    advance program

---
+++ 167.HttpClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        HttpClient client = new HttpClient();
+
+        string url = "https://jsonplaceholder.typicode.com/posts/1";
+        string response = await client.GetStringAsync(url);
+
+        Console.WriteLine(response);
+    }
+}
