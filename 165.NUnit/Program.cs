7b084caac3d92a4e995f506d88c16c1fd6174a57
shrinivasmunjale <shrinivasmunjale3@gmail.com>
2026-04-30 09:34:16 +0530

    advance program

---
+++ 165.NUnit/Program.cs
@@ -0,0 +1,27 @@
+﻿//Unit Testing using NUnit
+
+using NUnit.Framework;
+
+namespace DemoTest
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Add_Test()
+        {
+            Calculator calc = new Calculator();
+            int result = calc.Add(2, 3);
+
+            Assert.AreEqual(5, result);
+        }
+    }
+}
