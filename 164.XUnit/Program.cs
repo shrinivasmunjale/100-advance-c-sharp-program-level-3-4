ff318c47401a875f72a961bd846a47b4e3d3c575
shrinivasmunjale <shrinivasmunjale3@gmail.com>
2026-04-30 09:34:01 +0530

    advance program

---
+++ 164.XUnit/Program.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+public class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_ReturnsCorrectSum()
+    {
+        Calculator calc = new Calculator();
+
+        int result = calc.Add(2, 3);
+
+        Assert.Equal(5, result);
+    }
+}
