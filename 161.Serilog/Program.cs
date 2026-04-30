136773b37dff8fcc1e0e93724b4f3242b56e0af1
shrinivasmunjale <shrinivasmunjale3@gmail.com>
2026-04-30 09:25:30 +0530

    level 4 code

---
+++ 161.Serilog/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Serilog;
+
+class Program
+{
+    static void Main()
+    {
+        // Configure Logger
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        Log.Information("Application Started");
+
+        try
+        {
+            int a = 10, b = 0;
+            int result = a / b; // error
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "Exception occurred");
+        }
+
+        Log.Information("Application Ended");
+
+        Log.CloseAndFlush();
+    }
+}
