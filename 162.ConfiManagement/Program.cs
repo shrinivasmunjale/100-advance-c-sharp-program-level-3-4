deff86c7a5d5f79e97bc359a8a2657c9fd26b5c1
shrinivasmunjale <shrinivasmunjale3@gmail.com>
2026-04-30 09:32:32 +0530

    advance program

---
+++ 162.ConfiManagement/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        string appName = config["AppSettings:AppName"];
+        string version = config["AppSettings:Version"];
+
+        Console.WriteLine("App Name: " + appName);
+        Console.WriteLine("Version: " + version);
+    }
+}
