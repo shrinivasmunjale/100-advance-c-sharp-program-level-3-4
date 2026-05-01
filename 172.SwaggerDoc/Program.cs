e24fdfc0ce84131a0e84631c85c1eba2e88ed0ef
shrinivasmunjale <shrinivasmunjale3@gmail.com>
2026-05-01 20:44:12 +0530

    advance program

---
+++ 172.SwaggerDoc/Program.cs
@@ -0,0 +1,16 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+// Add Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Enable Swagger
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+app.Run();
